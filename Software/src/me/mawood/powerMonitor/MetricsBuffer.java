3d4bd1c9a58f0563df01437d78a95263cbf00fb1
gjwo <grahamjwood@gmail.com>
2017-08-23 15:12:32 +0100

    Added noChannels to Metrics Buffer to make it more generic

--- Software/src/me/mawood/powerMonitor/MetricsBuffer.java
+++ Software/src/me/mawood/powerMonitor/MetricsBuffer.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 
 class MetricsBuffer
 {
-    private static final byte asciiChar = 'V';
     private double rmsVoltage;
     private final short[] channelNumber = new short[9];
     private final double[] realPowerValues = new double[9];
     private final double[] apparentPowerValues = new double[9];
+    private final int noChannels;
 
     /**
      * MetricsBuffer    Translates a serial message in bytes to the metrics, the message is expected to contain
@@ -24,11 +24,13 @@ class MetricsBuffer
      */
     MetricsBuffer(byte[] msg)throws IllegalArgumentException
     {
+        final byte asciiChar = 'V';
+        noChannels =9;
         ByteBuffer bBuff = ByteBuffer.wrap(msg);
         if (bBuff.limit()<162) {throw new IllegalArgumentException("Message too short");}
         if (bBuff.get() != asciiChar) {throw new IllegalArgumentException("Illegal start character");}
         rmsVoltage = bBuff.getDouble();
-        for (int i=0; i<=8; i++)
+        for (int i = 0; i< noChannels; i++)
         {
          channelNumber[i] = bBuff.getShort();
          if (channelNumber[i] != i) {throw new IllegalArgumentException("Illegal channel number");}
@@ -40,6 +42,7 @@ class MetricsBuffer
     double getRmsVoltage(){return this.rmsVoltage;}
     double getRealPower(int channel){return this.realPowerValues[channel];}
     double getApparentPower(int channel){return this.apparentPowerValues[channel];}
+    int getNoChannels() {return this.noChannels;}
 
     void printMetricsBuffer()
     {
@@ -55,6 +58,7 @@ class MetricsBuffer
     {
         return "MetricsBuffer{" +
                 "rmsVoltage=" + rmsVoltage +
+                ", noChannels=" + noChannels +
                 ", channelNumber=" + Arrays.toString(channelNumber) +
                 ", realPowerValues=" + Arrays.toString(realPowerValues) +
                 ", apparentPowerValues=" + Arrays.toString(apparentPowerValues) +
