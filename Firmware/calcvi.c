3abff81f983001f241e9e04df9f53e7e956a3a19
gjwo <grahamjwood@gmail.com>
2017-08-27 11:46:02 +0100

    Clean up firmware comments and layout

--- Firmware/calcvi.c
+++ Firmware/calcvi.c
@@ -10,15 +10,17 @@ bool lastVCross,checkVCross;
 
 // Modified version of a method from https://github.com/openenergymonitor/EmonLib/blob/master/EmonLib.cpp
 // Calculator credits to the openenergymonitor project (https://github.com/openenergymonitor)
+// Current clamp and AC/AC voltage source scaling and calibration is done outside of the firmware so that
+// different external devices can be used.
 void calcVI(const unsigned int crossings)
 {
-  unsigned int crossCount = 0;
-  unsigned int numberOfSamples = 0;
-	double VoltsPerCount;
-	int i;
+    unsigned int crossCount = 0;
+    unsigned int numberOfSamples = 0;
+    Double VoltsPerCount;
+    int i;
 	
-  //Reset accumulators
-  sumV = 0;
+    //Reset accumulators
+    sumV = 0;
 	for(i = 0; i < HARDWARE_CHANNEL_NUM; i++)
 	{
 		sumI[i] = 0;
@@ -27,49 +29,47 @@ void calcVI(const unsigned int crossings)
 	
 	do
 	{
-    startV = readChannel(VOLTAGE_CHANNEL);
-		// keep trying until voltage is within 5% of a crossing point (offset zero)
+      startV = readChannel(VOLTAGE_CHANNEL);
+	  // keep trying until voltage is within 5% of a crossing point (offset zero)
 	} while (!((startV<(ADC_COUNTS*0.55))&&(startV>(ADC_COUNTS*0.45)))); 
 	
 	while(crossCount < crossings)
 	{
-		
-    numberOfSamples++;
-    lastFilteredV = filteredV;
-		
-    sampleV = readChannel(VOLTAGE_CHANNEL);
-		
+        numberOfSamples++;
+
+        // read voltage ADC
+        sampleV = readChannel(VOLTAGE_CHANNEL);
+        // read current ADCs
 		for(i = 0; i < HARDWARE_CHANNEL_NUM; i++)
-		{
 			sampleI[i] = readChannel(CHANNELS[i]);
-		}
-		
-    offsetV = offsetV + ((sampleV-offsetV)/1024);
-    filteredV = sampleV - offsetV;
-    sumV += filteredV * filteredV;
-    phaseShiftedV = lastFilteredV + PHASECAL * (filteredV - lastFilteredV);
+
+        lastFilteredV = filteredV;
+        // low pass filter to move sample voltage to +/- 1.65v scale rather than 0-3.3v scale
+        offsetV = offsetV + ((sampleV-offsetV)/1024);
+        filteredV = sampleV - offsetV;
+
+        sumV += filteredV * filteredV;
+
+        phaseShiftedV = lastFilteredV + PHASECAL * (filteredV - lastFilteredV);
 		
 		for(i = 0; i < HARDWARE_CHANNEL_NUM; i++)
 		{
+            // low pass filter to move sample voltage to +/- 1.65v scale rather than 0-3.3v scale
 			offsetI = offsetI + ((sampleI[i]-offsetI)/1024);
 			filteredI = sampleI[i] - offsetI;
 			sumI[i] += filteredI * filteredI;
-		
 			instP = phaseShiftedV * filteredI;
 			sumP[i] +=instP;
 		}
-		
-    lastVCross = checkVCross;
-		
+		// check for a voltage zero crossing i.e completion of a half wave and increment cross count if so
+        lastVCross = checkVCross;
 		checkVCross = sampleV > startV;
-		
-    if (numberOfSamples==1) lastVCross = checkVCross;
-
-    if (lastVCross != checkVCross) crossCount++;
-}
-
+        if (numberOfSamples==1) lastVCross = checkVCross;
+        if (lastVCross != checkVCross) crossCount++;
+    }
+    // post loop calculations
 	VoltsPerCount = VCC / ADC_COUNTS;
-  Vrms = VoltsPerCount * sqrt(sumV / numberOfSamples);
+    Vrms = VoltsPerCount * sqrt(sumV / numberOfSamples);
 
 	for(i = 0; i < HARDWARE_CHANNEL_NUM; i++)
 	{
