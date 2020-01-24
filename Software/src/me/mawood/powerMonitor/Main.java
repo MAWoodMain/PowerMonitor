088f85d89fbe6fa650bed4de14657c9a755b7021
Graham Wood <grahamjwood@gmail.com>
2020-01-24 11:40:04 +0000

    Added variables to control enablement of MQTT and API publishers

--- Software/src/me/mawood/powerMonitor/Main.java
+++ Software/src/me/mawood/powerMonitor/Main.java
@@ -3,13 +3,13 @@ package me.mawood.powerMonitor;
 import me.mawood.powerMonitor.circuits.Circuit;
 import me.mawood.powerMonitor.circuits.HomeCircuits;
 import me.mawood.powerMonitor.metrics.PowerMetricCalculator;
-import me.mawood.powerMonitor.publishers.PowerDataAPIPublisher;
-import me.mawood.powerMonitor.publishers.PowerDataMQTTPublisher;
+import me.mawood.powerMonitor.packets.STM8PacketCollector;
 import me.mawood.powerMonitor.packets.monitors.CurrentMonitor;
 import me.mawood.powerMonitor.packets.monitors.RealPowerMonitor;
 import me.mawood.powerMonitor.packets.monitors.VoltageMonitor;
 import me.mawood.powerMonitor.packets.monitors.configs.VoltageSenseConfig;
-import me.mawood.powerMonitor.packets.STM8PacketCollector;
+import me.mawood.powerMonitor.publishers.PowerDataAPIPublisher;
+import me.mawood.powerMonitor.publishers.PowerDataMQTTPublisher;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
 import java.io.IOException;
@@ -17,8 +17,32 @@ import java.util.HashMap;
 
 public class Main
 {
-    public static void main(String[] args) throws IOException, InterruptedException
+    static boolean enable_MQTT = true;
+    static boolean enable_API = false;
+
+    public static boolean isEnable_MQTT()
     {
+        return enable_MQTT;
+    }
+
+    public static boolean isEnable_API()
+    {
+        return enable_API;
+    }
+
+    public void setEnable_MQQT(boolean mqtt)
+    {
+        enable_MQTT = mqtt;
+    }
+
+    public void setEnable_API(boolean api)
+    {
+        enable_API = api;
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+
         STM8PacketCollector packetCollector = new STM8PacketCollector(1000);
         //packetCollector.addPacketEventListener(System.out::println);
         VoltageMonitor vm = new VoltageMonitor(1000, VoltageSenseConfig.UK9V, packetCollector);
@@ -31,18 +55,19 @@ public class Main
                     new CurrentMonitor(1000, circuit.getClampConfig(), circuit.getChannelNumber(), packetCollector),
                     new RealPowerMonitor(1000, VoltageSenseConfig.UK9V, circuit.getClampConfig(), circuit.getChannelNumber(), packetCollector)));
         }
-        //PowerDataAPIPublisher powerDataDataBaseUpdater = new PowerDataAPIPublisher(circuitMap);
-        //powerDataDataBaseUpdater.start();
-
-        PowerDataMQTTPublisher powerDataMQTTPublisher;
-        try
-        {
-            powerDataMQTTPublisher = new PowerDataMQTTPublisher(circuitMap);
-            powerDataMQTTPublisher.start(); // run in separate thread
-        } catch (MqttException e)
-        {
-            PowerDataMQTTPublisher.handleMQTTException(e);
-            System.exit(9);
+        if (enable_API) {
+            PowerDataAPIPublisher powerDataDataBaseUpdater = new PowerDataAPIPublisher(circuitMap);
+            powerDataDataBaseUpdater.start();
+        }
+        if (enable_MQTT) {
+            PowerDataMQTTPublisher powerDataMQTTPublisher;
+            try {
+                powerDataMQTTPublisher = new PowerDataMQTTPublisher(circuitMap);
+                powerDataMQTTPublisher.start(); // run in separate thread
+            } catch (MqttException e) {
+                PowerDataMQTTPublisher.handleMQTTException(e);
+                System.exit(9);
+            }
         }
     }
 }
