ea328371e6ee6a0987b0b0bdb80660cbd0edb69f
MAWood <mawoodmain@googlemail.com>
2020-01-27 20:11:50 +0000

    Re shuffled channels to match silkscreen.

--- Firmware/adc.c
+++ Firmware/adc.c
@@ -1,6 +1,19 @@
 #include "adc.h"
 
-unsigned char CHANNELS[] = {18,17,16,15,14,13,12,11,4};
+//unsigned char CHANNELS[] = {18,17,16,15,14,13,12,11,4};
+unsigned char CHANNELS[] = {17, 16, 15, 14, 4, 11, 12, 13, 18};
+// 18 -> PB0 (channel 1) SS 9
+// 17 -> PB1 (channel 2) SS 1
+// 16 -> PB2 (channel 3) SS 2
+// 15 -> PB3 (channel 4) SS 3
+// 14 -> PB4 (channel 5) SS 4
+// 13 -> PB5 (channel 6) SS 8
+// 12 -> PB6 (channel 7) SS 7
+// 11 -> PB7 (channel 8) SS 6
+//  4 -> PC4 (channel 9) SS 5
+// Ordering clockwise starting fromtop right (text upwards)
+// 1, 6, 7, 8, 9, 5, 4, 3, 2
+
 unsigned char VOLTAGE_CHANNEL = 22;
 
 void ADC_INIT()
