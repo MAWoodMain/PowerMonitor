9911c9664480a06283b7d937b2c28e83edc34b5c
Graham Wood <grahamjwood@gmail.com>
2020-01-30 14:40:52 +0000

    Attempt to get string from Q

--- Software/src/me/mawood/powerMonitor/publishers/MQTTHandler.java
+++ Software/src/me/mawood/powerMonitor/publishers/MQTTHandler.java
@@ -86,7 +86,7 @@ public class MQTTHandler implements MqttCallback
     @Override
     public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception
     {
-        String payload = new String(mqttMessage.getPayload());
+        String payload = String.valueOf(mqttMessage.getPayload());
         loggingQ.add("MQTT msg received Topic: " + topic + " Message: " + payload);
         System.out.println("MQTT msg received Topic: " + topic + " Message: " +payload);
         String[] subtopics = topic.split("/");
