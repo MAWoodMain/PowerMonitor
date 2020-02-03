97c94368051018cb77d46d420866e74fb2ee6f5f
Graham Wood <grahamjwood@gmail.com>
2020-02-03 10:54:40 +0000

    Revert "Stop storing energy for disabled circuits, updated jar"
    This reverts commit eaa5a9758d9c08d5610518ad79b8033a9b95b197.
    
    # Conflicts:
    #	Software/src/me/mawood/powerMonitor/Main.java
    #	Software/src/me/mawood/powerMonitor/circuits/EnergyStore.java

--- Software/src/me/mawood/powerMonitor/metrics/Metric.java
+++ Software/src/me/mawood/powerMonitor/metrics/Metric.java
@@ -2,30 +2,38 @@ package me.mawood.powerMonitor.metrics;
 
 public enum Metric
 {
-    VOLTS("Voltage",1.0,"V", "Volts"),
-    MILLI_VOLTS("Voltage",0.001,"mV", "milli-volts"),
-    AMPS("Current",1.0,"A", "Amps"),
-    MILLI_AMPS("Current",0.001,"mA", "milli-amps"),
-    WATT_HOURS("Energy",1.0,"WH", "Watt Hours"),
-    KILOWATT_HOURS("Energy",1000.0,"kWH", "KiloWatt Hours"),
-    WATTS("Real",1.0,"W","Watts"),
-    KILOWATT("Real",1000.0,"KW", "Kilowatt"),
-    VA("Apparent", 1.0,"VA", "Volt Amperes"),
-    VAR("Reactive", 1.0,"VAR", "Volt Amperes Reactive"),
-    POWERFACTOR("PowerFactor", 1.0,"PF","Power Factor");
+    VOLTS(MetricType.VOLTAGE,"Voltage",1.0,"V", "Volts"),
+    MILLI_VOLTS(MetricType.VOLTAGE,"Voltage",0.001,"mV", "milli-volts"),
+    AMPS(MetricType.CURRENT,"Current",1.0,"A", "Amps"),
+    MILLI_AMPS(MetricType.CURRENT,"Current",0.001,"mA", "milli-amps"),
+    WATT_HOURS(MetricType.ENERGY,"Energy",1.0,"WH", "Watt Hours"),
+    KILOWATT_HOURS(MetricType.ENERGY,"Energy",1000.0,"kWH", "KiloWatt Hours"),
+    WATTS(MetricType.POWER,"Real",1.0,"W","Watts"),
+    VA(MetricType.POWER,"Apparent", 1.0,"VA", "Volt Amperes"),
+    VAR(MetricType.POWER,"Reactive", 1.0,"VAR", "Volt Amperes Reactive"),
+    KILOWATT(MetricType.POWER,"Real",1000.0,"KW", "Kilowatt"),
+    POWERFACTOR(MetricType.POWER_FACTOR,"PowerFactor", 1.0,"PF","Power Factor");
 
+    final MetricType metricType;
     final String metricName;
     final double factor;
     final String symbol;
     final String unitName;
 
-    Metric(String metricName, double factor, String symbol, String unitName)
+    Metric(MetricType metricType, String metricName, double factor, String symbol, String unitName)
     {
+        this.metricType = metricType;
         this.metricName = metricName;
         this.factor = factor;
         this.symbol = symbol;
         this.unitName = unitName;
     }
+
+    public MetricType getType()
+    {
+        return MetricType.CURRENT;
+    }
+
     public String getUnitName()
     {
         return unitName;
@@ -46,4 +54,8 @@ public enum Metric
         return factor;
     }
 
+    public MetricType getMetricType()
+    {
+        return metricType;
+    }
 }
---
+++ Software/src/me/mawood/powerMonitor/metrics/MetricType.java
@@ -0,0 +1,10 @@
+package me.mawood.powerMonitor.metrics;
+
+public enum MetricType
+{
+    CURRENT,
+    VOLTAGE,
+    POWER,
+    ENERGY,
+    POWER_FACTOR
+}
