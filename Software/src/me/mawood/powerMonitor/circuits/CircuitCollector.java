dab3732a6c58f52f36a5fbed2fdb3b307cfccdee
Graham Wood <grahamjwood@gmail.com>
2020-02-04 15:39:29 +0000

    Add miing "," in json, update jar

--- Software/src/me/mawood/powerMonitor/circuits/CircuitCollector.java
+++ Software/src/me/mawood/powerMonitor/circuits/CircuitCollector.java
@@ -87,7 +87,7 @@ public class CircuitCollector extends Thread
             String jsonReadings =
                     "{\"Time\":\"" + energy.getTimestamp().toString() + "\"," +
                             "\"Readings\":{" +
-                            "\"Energy\":" + energy.getValue().toString()  +
+                            "\"Energy\":" + energy.getValue().toString() + ","+
                             "\"CumulativeEnergy\":" + cumulativeEnergy.getValue().toString()  +
                             "}}";
             mqttHandler.publishToBroker(subTopic, jsonReadings);
