aae5d872adc4dad3e56ed945ca783c0eaa15c559
Graham Wood <grahamjwood@gmail.com>
2020-02-06 10:28:59 +0000

    deal with warnings

--- Software/src/main/java/org/ladbury/powerMonitor/Main.java
+++ Software/src/main/java/org/ladbury/powerMonitor/Main.java
@@ -23,50 +23,32 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class Main
 {
 
-    private static boolean enable_MQTT = true;
-    private static boolean enable_API = false;
     private static HashMap<Circuit, PowerMetricCalculator> circuitMap = new HashMap<>();
     private static VoltageMonitor vm;
     private static STM8PacketCollector packetCollector;
     private static MQTTHandler mqttHandler;
-    private static CommandProcessor commandProcessor;
     private static LinkedBlockingQueue<String> commandQ;
     private static LinkedBlockingQueue<String> loggingQ;
-    private static PMLogger logger;
-    private static CircuitCollector circuitCollector;
-    private static EnergyBucketFiller bucketfiller;
 
-    // Getters and Setters
-    public static boolean isEnabled_MQTT()
+    public static HashMap<Circuit, PowerMetricCalculator> getCircuitMap()
     {
-        return enable_MQTT;
-    }
-    public static void enable_MQQT()
-    {
-        enable_MQTT = true;
-    }
-    public static void disable_MQQT()
-    {
-        enable_MQTT = false;
+        return circuitMap;
     }
 
-    public static boolean isEnabled_API()
+    public static MQTTHandler getMqttHandler()
     {
-        return enable_API;
-    }
-    public static void enable_API()
-    {
-        enable_API = true;
-    }
-    public static void disable_API()
-    {
-        enable_API = false;
+        return mqttHandler;
     }
 
-    public static HashMap<Circuit, PowerMetricCalculator> getCircuitMap() {return circuitMap;}
-    public static MQTTHandler getMqttHandler() {return mqttHandler;}
-    public static LinkedBlockingQueue<String>  getCommandQ() {return commandQ;}
-    public static LinkedBlockingQueue<String>  getLoggingQ() {return loggingQ;}
+    public static LinkedBlockingQueue<String> getCommandQ()
+    {
+        return commandQ;
+    }
+
+    public static LinkedBlockingQueue<String> getLoggingQ()
+    {
+        return loggingQ;
+    }
 
 
     public static void enableCollection(Circuit circuit)
@@ -74,14 +56,15 @@ public class Main
         circuitMap.put(
                 circuit,
                 new PowerMetricCalculator(vm,
-                new CurrentMonitor(1000, circuit.getClampConfig(), circuit.getChannelNumber(), packetCollector),
-                new RealPowerMonitor(1000, VoltageSenseConfig.UK9V, circuit.getClampConfig(), circuit.getChannelNumber(), packetCollector)));
-        loggingQ.add("Monitoring circuit "+circuit.getDisplayName());
+                        new CurrentMonitor(1000, circuit.getClampConfig(), circuit.getChannelNumber(), packetCollector),
+                        new RealPowerMonitor(1000, VoltageSenseConfig.UK9V, circuit.getClampConfig(), circuit.getChannelNumber(), packetCollector)));
+        loggingQ.add("Monitoring circuit " + circuit.getDisplayName());
     }
+
     public static void disableCollection(Circuit circuit)
     {
         circuitMap.remove(circuit);
-        loggingQ.add("Not monitoring circuit "+circuit.getDisplayName());
+        loggingQ.add("Not monitoring circuit " + circuit.getDisplayName());
     }
 
     public static void main(String[] argv) throws IOException
@@ -97,31 +80,29 @@ public class Main
         int energyBucketInterval = 5; // Minutes
 
         //if required enable publishing processes
-        if (isEnabled_MQTT()) {
-            try {
-                loggingQ.add("Enabling MQTT");
-                mqttHandler = new MQTTHandler(getLoggingQ(), getCommandQ());
-            } catch (MqttException e) {
-                MQTTHandler.handleMQTTException(e);
-                System.exit(9);
-            }
+        try {
+            loggingQ.add("Enabling MQTT");
+            mqttHandler = new MQTTHandler(getLoggingQ(), getCommandQ());
+        } catch (MqttException e) {
+            MQTTHandler.handleMQTTException(e);
+            System.exit(9);
         }
 
         // set up & start support processes
-        logger = new PMLogger(loggingQ);
+        PMLogger logger = new PMLogger(loggingQ);
         logger.start();
         loggingQ.add("Enabled Logger");
         loggingQ.add("Enabling CommandProcessor");
-        commandProcessor = new CommandProcessor(getCommandQ(),getLoggingQ());
+        CommandProcessor commandProcessor = new CommandProcessor(getCommandQ(), getLoggingQ());
         commandProcessor.start();
 
         loggingQ.add("Enabling CircuitCollector");
         boolean[] circuitRequired = {false, false, false, false, false, false, false, false, false, true}; // 0-9 0 not used, 9 is Whole_House
-        circuitCollector = new CircuitCollector(getCircuitMap(), mqttHandler,5,getLoggingQ());
+        CircuitCollector circuitCollector = new CircuitCollector(getCircuitMap(), mqttHandler, 5, getLoggingQ());
 
 
         loggingQ.add("Enabling EnergyBucketFiller");
-        bucketfiller = new EnergyBucketFiller(energyBucketInterval,true,circuitCollector,getLoggingQ());
+        EnergyBucketFiller bucketfiller = new EnergyBucketFiller(energyBucketInterval, true, circuitCollector, getLoggingQ());
         bucketfiller.start();
 
         // Start packet collection
@@ -131,8 +112,7 @@ public class Main
         loggingQ.add("Enabling VoltageMonitor ");
         vm = new VoltageMonitor(1000, VoltageSenseConfig.UK9V, packetCollector);
         // Enable interpretation for required circuits
-        for(Circuit circuit: HomeCircuits.values())
-        {
+        for (Circuit circuit : HomeCircuits.values()) {
             if (circuitRequired[circuit.getChannelNumber()])
                 enableCollection(circuit);
         }
