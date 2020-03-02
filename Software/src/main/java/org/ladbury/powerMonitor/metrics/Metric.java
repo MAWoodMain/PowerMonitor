84fac65d9170ca87e8e068c77864d6a5c3d5f294
Graham Wood <grahamjwood@gmail.com>
2020-03-02 16:35:16 +0000

    Remove redundant metricType, update Jar

--- Software/src/main/java/org/ladbury/powerMonitor/metrics/Metric.java
+++ Software/src/main/java/org/ladbury/powerMonitor/metrics/Metric.java
@@ -2,28 +2,28 @@ package org.ladbury.powerMonitor.metrics;
 
 public enum Metric
 {
-    VOLTS(MetricType.VOLTAGE,"Voltage",1.0,"V", "Volts"),
-    MILLI_VOLTS(MetricType.VOLTAGE,"Voltage",0.001,"mV", "milli-volts"),
-    AMPS(MetricType.CURRENT,"Current",1.0,"A", "Amps"),
-    MILLI_AMPS(MetricType.CURRENT,"Current",0.001,"mA", "milli-amps"),
-    WATT_HOURS(MetricType.ENERGY,"Energy",1.0,"WH", "Watt Hours"),
-    KILOWATT_HOURS(MetricType.ENERGY,"Energy",1000.0,"kWH", "KiloWatt Hours"),
-    WATTS(MetricType.POWER,"Real",1.0,"W","Watts"),
-    VA(MetricType.POWER,"Apparent", 1.0,"VA", "Volt Amperes"),
-    VAR(MetricType.POWER,"Reactive", 1.0,"VAR", "Volt Amperes Reactive"),
-    KILOWATT(MetricType.POWER,"Real",1000.0,"KW", "Kilowatt"),
-    POWERFACTOR(MetricType.POWER_FACTOR,"PowerFactor", 1.0,"PF","Power Factor");
+    VOLTS("Voltage",1.0,"V", "Volts"),
+    MILLI_VOLTS("Voltage",0.001,"mV", "milli-volts"),
+    AMPS("Current",1.0,"A", "Amps"),
+    MILLI_AMPS("Current",0.001,"mA", "milli-amps"),
+    WATT_HOURS("Energy",1.0,"WH", "Watt Hours"),
+    KILOWATT_HOURS("Energy",1000.0,"kWH", "KiloWatt Hours"),
+    WATTS("Real",1.0,"W","Watts"),
+    VA("Apparent", 1.0,"VA", "Volt Amperes"),
+    VAR("Reactive", 1.0,"VAR", "Volt Amperes Reactive"),
+    KILOWATT("Real",1000.0,"KW", "Kilowatt"),
+    POWERFACTOR("PowerFactor", 1.0,"PF","Power Factor");
 
-    final MetricType metricType;
-    final String metricName;
-    final double factor;
-    final String symbol;
-    final String unitName;
+
+    private final String metricName;
+    private final double factor;
+    private final String symbol;
+    private final String unitName;
 
     //Constructor
-    Metric(MetricType metricType, String metricName, double factor, String symbol, String unitName)
+    Metric( String metricName, double factor, String symbol, String unitName)
     {
-        this.metricType = metricType;
+
         this.metricName = metricName;
         this.factor = factor;
         this.symbol = symbol;
@@ -46,9 +46,5 @@ public enum Metric
     public double getFactor()
     {
         return factor;
-    }
-    public MetricType getMetricType()
-    {
-        return metricType;
     }
 }
