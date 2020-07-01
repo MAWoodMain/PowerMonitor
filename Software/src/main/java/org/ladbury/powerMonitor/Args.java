2b026efac2ee88cb1eaaef846f9ead487680f8cc
Graham Wood <grahamjwood@gmail.com>
2020-07-01 14:34:25 +0100

    Add default argument initialisation, change default MQTT server, update Jar

--- Software/src/main/java/org/ladbury/powerMonitor/Args.java
+++ Software/src/main/java/org/ladbury/powerMonitor/Args.java
@@ -7,13 +7,13 @@ public class Args
     // Command line parameters
     @SuppressWarnings("SpellCheckingInspection")
     @Parameter(names={"--mqqtserver", "-m"})
-    private String mqttServer;
+    private String mqttServer = "10.0.128.20";
     @Parameter(names={"--ClientName", "-c"})
-    private String clientName;
+    private String clientName = "Pmon10";
     @Parameter(names={"--interval", "-i"})
-    private int accumulationInterval;
+    private int accumulationInterval = 5;
     @Parameter(names={"LoggingLevel","-l"})
-    private String loggingLevelStr;
+    private String loggingLevelStr = "info";
     @Parameter(names = {"--help","-h"}, help = true)
     private boolean help;
 
